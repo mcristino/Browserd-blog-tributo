3c2b8dae4287074a1a24395b832383c5f297cd15
Miguel Cristino <mcristino23@gmail.com>
2024-03-15 21:59:43 +0000

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+// Configuração do dotenv (para encriptar os dados de acesso a base de dados)
+dotenv.config();
+
+// Ligação a base de dados MongoDb
+mongoose.connect(
+    process.env.MONGO)
+    .then(() => {
+		console.log('Ligado a MongoDb com sucesso!');
+	})
+    .catch((err) => {
+        console.log('Não foi possivel ligar a MongoDb!', err);
+    })
 
 const app = express();
 
 app.listen(3000, () => {
-	console.log('Server is running on port 3000!');
+	console.log('O servidor esta a correr na porta 3000!');
 });
