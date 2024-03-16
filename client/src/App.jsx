914cf93d2e58db810de37653c12e74456f3dda97
Miguel Cristino <mcristino23@gmail.com>
2024-03-16 19:04:16 +0000

    private dashboard for backoffice

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
 	return (
@@ -15,10 +16,12 @@ export default function App() {
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/about' element={<About />} />
-				<Route path='/dashboard' element={<Dashboard />} />
 				<Route path='/projects' element={<Projects />} />
 				<Route path='/signin' element={<SignIn />} />
 				<Route path='/signup' element={<SignUp />} />
+				<Route element={<PrivateRoute />}>
+					<Route path='/dashboard' element={<Dashboard />} />
+				</Route>
 			</Routes>
 			<Footer />
 		</BrowserRouter>
