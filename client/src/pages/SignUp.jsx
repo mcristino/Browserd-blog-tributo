f2e88b53b84b306040baf5179079a904207933cf
Miguel Cristino <mcristino23@gmail.com>
2024-03-16 01:52:50 +0000

    signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,44 @@
-import { Button, Label, TextInput } from 'flowbite-react';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+	// Guardar os dados do formulário
+	const [formData, setFormData] = useState({});
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
+	const handleChange = e => {
+		setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+	};
+	const handleSubmit = async e => {
+		e.preventDefault();
+		if (!formData.username || !formData.email || !formData.password) {
+			return setErrorMessage('Preencha todos os campos.');
+		}
+		try {
+			setLoading(true);
+			setErrorMessage(null);
+			const res = await fetch('/api/auth/signup', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			if (data.success === false) {
+				return setErrorMessage(data.message);
+			}
+			setLoading(false);
+			if (res.ok) {
+				navigate('/signin');
+			}
+		} catch (error) {
+			setErrorMessage(error.message);
+			setLoading(false);
+		}
+	};
+
 	return (
 		<div className='min-h-screen mt-20'>
 			<div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -12,25 +48,31 @@ export default function SignUp() {
 						<span className='px-2 py-1 bg-gradient-to-r from-red-600 to-orange-500 rounded-lg text-white'>Browserd's</span>
 						Blog
 					</Link>
-					<p className='text-sm mt-5'>Este é um projeto de demonstração e de Tributo a um grande blog Português. Pode registar-se com o seu email e palavra-passe ou com o Google.</p>
+					<p className='text-sm mt-5'>Este é um projeto de demonstração e de tributo a um grande blog Português. Pode registar-se com o seu email e palavra-passe ou com o Google.</p>
 				</div>
 				{/* Lado Direito */}
 				<div className='flex-1'>
-					<form className='flex flex-col gap-4 '>
+					<form className='flex flex-col gap-4' onSubmit={handleSubmit}>
 						<div>
 							<Label value='O seu nome de utilizador' />
-							<TextInput type='text' placeholder='Nome de Utilizador' id='username' />
+							<TextInput type='text' placeholder='Nome de Utilizador' id='username' onChange={handleChange} />
 						</div>
 						<div>
 							<Label value='O seu email' />
-							<TextInput type='text' placeholder='Email' id='email' />
+							<TextInput type='email' placeholder='Email' id='email' onChange={handleChange} />
 						</div>
 						<div>
 							<Label value='A sua palavra-passe' />
-							<TextInput type='text' placeholder='Palavra-pass' id='password' />
+							<TextInput type='password' placeholder='Palavra-pass' id='password' onChange={handleChange} />
 						</div>
-						<Button gradientDuoTone='pinkToOrange' type='submit'>
-							Registar
+						<Button gradientDuoTone='pinkToOrange' type='submit' disabled={loading}>
+							{loading ? (
+								<>
+									<Spinner size='sm' />,<span className='pl-3'>A carregar..</span>
+								</>
+							) : (
+								'Registar-se'
+							)}
 						</Button>
 					</form>
 					<div className='flex gap-2 text-sm mt-5'>
@@ -39,6 +81,11 @@ export default function SignUp() {
 							Iniciar sessão
 						</Link>
 					</div>
+					{errorMessage && (
+						<Alert className='mt-5' color='failure'>
+							{errorMessage}
+						</Alert>
+					)}
 				</div>
 			</div>
 		</div>
