fa65a9374227ba9314a97bf3b1dfc26f40643871
Miguel Cristino <mcristino23@gmail.com>
2024-03-17 21:46:03 +0000

    create post function

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,12 +6,15 @@ import { app } from '../firebase';
 import { useState } from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreatePost() {
 	const [file, setFile] = useState(null);
 	const [imageUploadProgress, setImageUploadProgress] = useState(null);
 	const [imageUploadError, setImageUploadError] = useState(null);
 	const [formData, setFormData] = useState({});
+	const [publishError, setPublishError] = useState(null);
+	const navigate = useNavigate();
 
 	// Carregar Imagem
 	const handleUploadImage = async () => {
@@ -50,13 +53,38 @@ export default function CreatePost() {
 		}
 	};
 
+	// Enviar publicação
+	const handleSubmit = async e => {
+		e.preventDefault();
+		try {
+			const res = await fetch('/api/post/create', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			if (!res.ok) {
+				setPublishError(data.message);
+				return;
+			}
+			if (res.ok) {
+				setPublishError(null);
+				navigate(`/post/${data.slug}`);
+			}
+		} catch (error) {
+			setPublishError('Não foi possível publicar');
+		}
+	};
+
 	return (
 		<div className='p-3 max-w-3xl mx-auto min-h-screen'>
 			<h1 className='text-center text-3xl my-7 font-semibold'>Criar uma publicação</h1>
-			<form className='flex flex-col gap-4'>
+			<form className='flex flex-col gap-4' onSubmit={handleSubmit}>
 				<div className='flex flex-col gap-4 sm:flex-row justify-between'>
-					<TextInput type='text' placeholder='Título' required id='title' className='flex-1' />
-					<Select>
+					<TextInput type='text' placeholder='Título' required id='title' className='flex-1' onChange={e => setFormData({ ...formData, title: e.target.value })} />
+					<Select onChange={e => setFormData({ ...formData, category: e.target.value })}>
 						<option value='uncategorized'>Selecione uma categoria</option>
 						<option value='pessoal'>Pessoal</option>
 						<option value='escrita'>Escrita</option>
@@ -98,10 +126,23 @@ export default function CreatePost() {
 				{imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
 				{formData.image && <img src={formData.image} alt='Imagem de publicação' className='w-full h-72 object-cover' />}
 
-				<ReactQuill theme='snow' placeholder='Escreva algo...' className='h-72 mb-12' required />
+				<ReactQuill
+					theme='snow'
+					placeholder='Escreva algo...'
+					className='h-72 mb-12'
+					required
+					onChange={value => {
+						setFormData({ ...formData, content: value });
+					}}
+				/>
 				<Button type='submit' className='bg-gradient-to-r from-red-600 to-orange-500'>
 					Publicar
 				</Button>
+				{publishError && (
+					<Alert className='mt-5' color='failure'>
+						{publishError}
+					</Alert>
+				)}
 			</form>
 		</div>
 	);
