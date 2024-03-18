c59df573a2809df86bd19daf59298e049c0d7a28
Miguel Cristino <mcristino23@gmail.com>
2024-03-18 20:44:17 +0000

    edit & delete comments & minor changes

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -2,9 +2,12 @@ import moment from 'moment';
 import { useEffect, useState } from 'react';
 import { FaThumbsUp } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
+import { Button, Textarea } from 'flowbite-react';
 
-export default function Comment({ comment, onLike }) {
+export default function Comment({ comment, onLike, onEdit, onDelete }) {
 	const [user, setUser] = useState({});
+	const [isEditing, setIsEditing] = useState(false);
+	const [editedContent, setEditedContent] = useState(comment.content);
 	const { currentUser } = useSelector(state => state.user);
 
 	useEffect(() => {
@@ -24,6 +27,31 @@ export default function Comment({ comment, onLike }) {
 		getUser();
 	}, [comment]);
 
+	// editar comentário
+	const handleEdit = () => {
+		setIsEditing(true);
+		setEditedContent(comment.content);
+	};
+
+	// Guardar comentário editado
+	const handleSave = async () => {
+		try {
+			const res = await fetch(`/api/comment/editComment/${comment._id}`, {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ content: editedContent }),
+			});
+			if (res.ok) {
+				setIsEditing(false);
+				onEdit(comment, editedContent);
+			}
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
 	return (
 		<div className='flex p-4 border-b dark:border-gray-600 text-sm'>
 			<div className='flex-shrink-0 mr-3 '>
@@ -34,13 +62,44 @@ export default function Comment({ comment, onLike }) {
 					<span className='font-bold mr-1 text-xs truncate'>{user ? `@${user.username}` : 'Utilizador anónimo'}</span>
 					<span className='text-gray-500 text-xs'>{moment(comment.createdAt).fromNow()}</span>
 				</div>
-				<p className='text-gray-500 pb-3'>{comment.content}</p>
-				<div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2 '>
-					<button type='button' onClick={() => onLike(comment._id)} className={`text-gray-400 hover:text-orange-400 ${currentUser && comment.likes.includes(currentUser._id) && '!text-orange-500'}`}>
-						<FaThumbsUp className='text-sm' />
-					</button>
-					<p className='text-gray-400'>{comment.numberOfLikes > 0 && comment.numberOfLikes + ' ' + (comment.numberOfLikes === 1 ? 'gosto' : 'gostos')}</p>
-				</div>
+
+				{isEditing ? (
+					<>
+						<Textarea className='mb-2' value={editedContent} onChange={e => setEditedContent(e.target.value)} />
+						<div className='flex justify-end gap-2 text-xs'>
+							<Button type='button' size='sm' onClick={handleSave} className='bg-gradient-to-r from-red-600 to-orange-500'>
+								Guardar
+							</Button>
+							<Button type='button' size='sm' outline onClick={() => setIsEditing(false)} className='bg-gradient-to-r from-orange-500 to-red-600'>
+								Cancelar
+							</Button>
+						</div>
+					</>
+				) : (
+					<>
+						<p className='text-gray-500 pb-3'>{comment.content}</p>
+						<div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2 '>
+							<button
+								type='button'
+								onClick={() => onLike(comment._id)}
+								className={`text-gray-400 hover:text-orange-400 ${currentUser && comment.likes.includes(currentUser._id) && '!text-orange-500'}`}
+							>
+								<FaThumbsUp className='text-sm' />
+							</button>
+							<p className='text-gray-400'>{comment.numberOfLikes > 0 && comment.numberOfLikes + ' ' + (comment.numberOfLikes === 1 ? 'gosto' : 'gostos')}</p>
+							{currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
+								<>
+									<button onClick={handleEdit} type='button' className='text-gray-400 hover:text-orange-400'>
+										Editar
+									</button>
+									<button onClick={() => onDelete(comment._id)} type='button' className='text-gray-400 hover:text-red-600'>
+										Apagar
+									</button>
+								</>
+							)}
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,17 +1,20 @@
-import { Alert, Button, Textarea } from 'flowbite-react';
+import { Alert, Button, Modal, Textarea } from 'flowbite-react';
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import Comment from './Comment';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { set } from 'mongoose';
 
 export default function CommentSection({ postId }) {
 	const { currentUser } = useSelector(state => state.user);
 	const [comment, setComment] = useState('');
 	const [commentError, setCommentError] = useState(null);
 	const [comments, setComments] = useState([]);
-	const navigate = useNavigate();
+	const [showModal, setShowModal] = useState(false);
+	const [commentToDelete, setCommentToDelete] = useState(null);
 
-	console.log(comments);
+	const navigate = useNavigate();
 
 	// Enviar comentário
 	const handleSubmit = async e => {
@@ -74,6 +77,31 @@ export default function CommentSection({ postId }) {
 		}
 	};
 
+	// Editar comentário && atualizar comentários
+	const handleEdit = async (comment, editedContent) => {
+		setComments(comments.map(c => (c._id === comment._id ? { ...c, content: editedContent } : c)));
+	};
+
+	// Apagar comentário
+	const handleDelete = async commentId => {
+		setShowModal(false);
+		try {
+			if (!currentUser) {
+				navigate('/signin');
+				return;
+			}
+			const res = await fetch(`/api/comment/deleteComment/${commentId}`, {
+				method: 'DELETE',
+			});
+			if (res.ok) {
+				const data = await res.json();
+				setComments(comments.filter(comment => comment._id !== commentId));
+			}
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
 	return (
 		<div className='max-w-2xl mx-auto w-full p-3'>
 			{currentUser ? (
@@ -97,7 +125,7 @@ export default function CommentSection({ postId }) {
 					<Textarea placeholder='Adiciona um comentário...' rows='3' maxLength='200' onChange={e => setComment(e.target.value)} value={comment} />
 					<div className='flex justify-between items-center mt-5'>
 						<p className='text-gray-500 text-xs'>Restam {200 - comment.length} caracteres</p>
-						<Button outline className='bg-gradient-to-r from-red-600 to-orange-500' type='submit'>
+						<Button outline className='bg-gradient-to-r from-orange-500 to-red-600' type='submit'>
 							Comentar
 						</Button>
 					</div>
@@ -119,10 +147,36 @@ export default function CommentSection({ postId }) {
 						</div>
 					</div>
 					{comments.map(comment => (
-						<Comment key={comment._id} comment={comment} onLike={handleLike} />
+						<Comment
+							key={comment._id}
+							comment={comment}
+							onLike={handleLike}
+							onEdit={handleEdit}
+							onDelete={commentId => {
+								setShowModal(true);
+								setCommentToDelete(commentId);
+							}}
+						/>
 					))}
 				</>
 			)}
+			<Modal show={showModal} onClose={() => setShowModal(false)} popup size='md'>
+				<Modal.Header />
+				<Modal.Body>
+					<div className='text-center'>
+						<HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+						<h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>Tem a certeza que deseja eliminar este comentário?</h3>
+					</div>
+					<div className='flex justify-center gap-4'>
+						<Button color='failure' onClick={() => handleDelete(commentToDelete)}>
+							Sim, tenho a certeza
+						</Button>
+						<Button color='gray' onClick={() => setShowModal(false)} outline>
+							Não, Cancelar
+						</Button>
+					</div>
+				</Modal.Body>
+			</Modal>
 		</div>
 	);
 }
