d39c09943c6bd0429bc5d42b28992f906a84f1cb
Miguel Cristino <mcristino23@gmail.com>
2024-03-18 00:48:39 +0000

    show more function to show more posts on posts tab

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,6 +6,8 @@ import { Link } from 'react-router-dom';
 export default function DashPosts() {
 	const { currentUser } = useSelector(state => state.user);
 	const [userPosts, setUserPosts] = useState([]);
+	const [showMore, setShowMore] = useState(true);
+
 	console.log(userPosts);
 
 	useEffect(() => {
@@ -15,6 +17,9 @@ export default function DashPosts() {
 				const data = await res.json();
 				if (res.ok) {
 					setUserPosts(data.posts);
+					if (data.posts.length < 9) {
+						setShowMore(false);
+					}
 				}
 			} catch (error) {}
 		};
@@ -22,6 +27,23 @@ export default function DashPosts() {
 			fetchPosts();
 		}
 	}, [currentUser._id]);
+
+	// Botão para mostrar mais publicações
+	const handleShowMore = async () => {
+		const startIndex = userPosts.length;
+		try {
+			const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+			const data = await res.json();
+			if (res.ok) {
+				setUserPosts(prev => [...prev, ...data.posts]);
+				if (data.posts.length < 9) {
+					setShowMore(false);
+				}
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	return (
 		<div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
@@ -65,6 +87,11 @@ export default function DashPosts() {
 							</Table.Body>
 						))}
 					</Table>
+					{showMore && (
+						<button onClick={handleShowMore} className='w-full text-orange-500 self-center text-sm py-7 font-semibold'>
+							Mostrar mais
+						</button>
+					)}
 				</>
 			) : (
 				<p>Ainda não tens publicações</p>
